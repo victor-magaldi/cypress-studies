6ec45088bf72a73cb6d1c9f9b125d3853c993814
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-09-06 20:48:17 -0300

    test: implement test open and close modal

--- project-02/cypress/e2e/task.cy.js
+++ project-02/cypress/e2e/task.cy.js
@@ -1,8 +1,18 @@
 describe('task management', () => {
   it('should open and close task modal', () => {
     cy.visit('http://localhost:5173')
+
     cy.get('button').contains('Add Task').click()
+    cy.get('.modal').should('exist')
     cy.get('.backdrop').click({ force: true })   // force true um clique mesmo que exista algo acima do elemento que quer clicar ex: um overlay
+    cy.get('.backdrop').should('not.exist')
+    cy.get('.modal').should('not.exist')
+
+    cy.get('button').contains('Add Task').click()
+    cy.get('.modal').should('exist')
+    cy.get('.modal').contains('Cancel').click()
+    cy.get('.backdrop').should('not.exist')
+    cy.get('.modal').should('not.exist')
   })
 
 })
