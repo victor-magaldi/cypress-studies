1671c492fd84cb8257ce34b92a4fbd2e81565bfb
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-09-06 20:09:56 -0300

    test: implement shound have.length

--- project-02/cypress/e2e/basics.cy.js
+++ project-02/cypress/e2e/basics.cy.js
@@ -5,6 +5,7 @@ describe('tasks page', () => {
   })
   it('should display the page title', () => {
     cy.visit('http://localhost:5173')
+    cy.get('.main-header h1').should("have.length", 1)
     cy.get('.main-header h1').contains('My Cypress Couse Tasks')
   })
 })
