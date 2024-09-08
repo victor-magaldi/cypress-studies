007aa282cc95c441febe8978576edb97153162ed
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-09-08 00:07:28 -0300

    test: should navigatite between pages

---
+++ project-03/cypress/e2e/navigation.cy.js
@@ -0,0 +1,12 @@
+describe('page navigation', () => {
+  it('should navigatite between pages', () => {
+    cy.visit('http://localhost:5173');
+    cy.get('[data-cy="header-about-link"]').click(); // this selector is one pattern
+    cy.location('pathname').should('eq', '/about');
+    cy.go('back');
+    cy.location('pathname').should('eq', '/');
+    cy.get('[data-cy="header-about-link"]').click();
+    cy.get('[data-cy="header-home-link"]').click();
+    cy.location('pathname').should('eq', '/');
+  });
+});
