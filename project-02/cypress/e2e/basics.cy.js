05da03322cdf685001b4603797f11c810249b3bb
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-09-07 23:14:32 -0300

    chore: files formated prettier

--- project-02/cypress/e2e/basics.cy.js
+++ project-02/cypress/e2e/basics.cy.js
@@ -1,12 +1,12 @@
 describe('tasks page', () => {
   it('should render the main image', () => {
-    cy.visit('http://localhost:5173')
-    cy.get('.main-header').find('img')   // não pode usar o get encadeado e find só se usa encadead
+    cy.visit('http://localhost:5173');
+    cy.get('.main-header').find('img'); // não pode usar o get encadeado e find só se usa encadead
     // cy.get('.main-header img')    // => também funcionaria
-  })
+  });
   it('should display the page title', () => {
-    cy.visit('http://localhost:5173')
-    cy.get('.main-header h1').should("have.length", 1)
-    cy.get('.main-header h1').contains('My Cypress Course Tasks')
-  })
-})
+    cy.visit('http://localhost:5173');
+    cy.get('.main-header h1').should('have.length', 1);
+    cy.get('.main-header h1').contains('My Cypress Course Tasks');
+  });
+});
--- project-02/cypress/e2e/task.cy.js
+++ project-02/cypress/e2e/task.cy.js
@@ -1,43 +1,52 @@
 describe('task management', () => {
   it('should open and close task modal', () => {
-    cy.visit('http://localhost:5173')
+    cy.visit('http://localhost:5173');
 
-    cy.get('button').contains('Add Task').click()
-    cy.get('.modal').should('exist')
-    cy.get('.backdrop').click({ force: true })   // force true um clique mesmo que exista algo acima do elemento que quer clicar ex: um overlay
-    cy.get('.backdrop').should('not.exist')
-    cy.get('.modal').should('not.exist')
+    cy.get('button').contains('Add Task').click();
+    cy.get('.modal').should('exist');
+    cy.get('.backdrop').click({ force: true }); // force true um clique mesmo que exista algo acima do elemento que quer clicar ex: um overlay
+    cy.get('.backdrop').should('not.exist');
+    cy.get('.modal').should('not.exist');
 
-    cy.get('button').contains('Add Task').click()
-    cy.get('.modal').should('exist')
-    cy.get('.modal').contains('Cancel').click()
-    cy.get('.backdrop').should('not.exist')
-    cy.get('.modal').should('not.exist')
-  })
+    cy.get('button').contains('Add Task').click();
+    cy.get('.modal').should('exist');
+    cy.get('.modal').contains('Cancel').click();
+    cy.get('.backdrop').should('not.exist');
+    cy.get('.modal').should('not.exist');
+  });
 
   it('should create a new task', () => {
-    const name = 'New Task'
-    const description = 'New Task'
+    const name = 'New Task';
+    const description = 'New Task';
 
-    cy.visit('http://localhost:5173')
-    cy.get('button').contains('Add Task').click()
-    cy.get('#title').type(name)
-    cy.get('#summary').type(description)
-    cy.get('.modal').contains('Add Task').click()
-    cy.get('.backdrop').should('not.exist')
-    cy.get('.modal').should('not.exist')
-    cy.get('.task').should('have.length', 1)
-    cy.get('.task h2').contains(name)
-    cy.get('.task p').contains(description)
-
-  })
+    cy.visit('http://localhost:5173');
+    cy.get('button').contains('Add Task').click();
+    cy.get('#title').type(name);
+    cy.get('#summary').type(description);
+    cy.get('.modal').contains('Add Task').click();
+    cy.get('.backdrop').should('not.exist');
+    cy.get('.modal').should('not.exist');
+    cy.get('.task').should('have.length', 1);
+    cy.get('.task h2').contains(name);
+    cy.get('.task p').contains(description);
+  });
 
   it('should validate user input', () => {
-    cy.visit('http://localhost:5173')
-    cy.get('button').contains('Add Task').click()
-    cy.get('.modal').contains('Add Task').click()
-    cy.get('.modal').contains('Add Task').click()
-    cy.get('.modal').contains('Please provide values for task title')
-  })
+    cy.visit('http://localhost:5173');
+    cy.get('button').contains('Add Task').click();
+    cy.get('.modal').contains('Add Task').click();
+    cy.get('.modal').contains('Add Task').click();
+    cy.get('.modal').contains('Please provide values for task title');
+  });
 
-})
+  it('should filter tasks', () => {
+    const name = 'New Task';
+    const description = 'New Task';
+
+    cy.visit('http://localhost:5173');
+    cy.get('button').contains('Add Task').click();
+    cy.get('#title').type(name);
+    cy.get('#summary').type(description);
+    cy.get('#category').select('🚨 Urgent');
+  });
+});
