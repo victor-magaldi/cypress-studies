6b2c0bd4de03d7e12a3968d035adc96043983960
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-09-08 22:21:44 -0300

    refactor: improve tests contact form

--- project-03/cypress/e2e/contact.cy.js
+++ project-03/cypress/e2e/contact.cy.js
@@ -6,10 +6,13 @@ describe('contact form', () => {
     );
     cy.get('[data-cy="contact-input-name"]').type('John Doe');
     cy.get('[data-cy="contact-input-email"]').type('test@example.com');
-    cy.get('[data-cy="contact-btn-submit"]').contains('Send Message');
+    cy.get('[data-cy="contact-btn-submit"]')
+      .contains('Send Message')
+      .and('not.have.attr', 'disabled'); // or should, 'and' improve readability
     cy.get('[data-cy="contact-btn-submit"]').click();
-    cy.get('[data-cy="contact-btn-submit"]').contains('Sending...');
-    cy.get('[data-cy="contact-btn-submit"]').should('have.attr', 'disabled'); // or .should('be.disabled')
+    cy.get('[data-cy="contact-btn-submit"]')
+      .contains('Sending...')
+      .should('have.attr', 'disabled'); // or .and('be.disabled')
     cy.wait(1000);
     cy.get('[data-cy="contact-btn-submit"]').contains('Send Message');
   });
