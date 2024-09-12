3fec3ed555f96893c55b9dc756cf3b5951abd310
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-09-11 21:41:52 -0300

    test: create and use stub

--- project-04/cypress/e2e/location.cy.js
+++ project-04/cypress/e2e/location.cy.js
@@ -1,8 +1,11 @@
-/// <reference types="cypress" />
-
 describe('share location', () => {
   it('should fetch the user location', () => {
-    cy.visit('/');
+    cy.visit('/').then((win) => {
+      cy.stub(win.navigator.geolocation, 'getCurrentPosition').as(
+        'getUserPosition'
+      );
+    });
     cy.get('[data-cy="get-loc-btn"]').click();
+    cy.get('@getUserPosition').should('have.been.called');
   });
 });
