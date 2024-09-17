fdd0ef1683e1ab8a7a0fe356f89f6b6a338f3df2
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-09-17 20:56:10 -0300

    test: validating value in stub clipBoard

--- project-04/cypress/e2e/location.cy.js
+++ project-04/cypress/e2e/location.cy.js
@@ -22,7 +22,6 @@ describe('share location', () => {
     cy.get('[data-cy="get-loc-btn"]').click();
     cy.get('@getUserPosition').should('have.been.called');
     cy.get('[data-cy="get-loc-btn"]').should('be.disabled');
-    cy.get('[data-cy="get-loc-btn"]').should('be.disabled');
     cy.get('[data-cy="actions"]').should('contain', 'Location fetched');
   });
 
@@ -31,5 +30,9 @@ describe('share location', () => {
     cy.get('[data-cy="get-loc-btn"').click();
     cy.get('[data-cy="share-loc-btn"').click();
     cy.get('@saveToClipboard').should('have.been.called');
+    cy.get('@saveToClipboard').should(
+      'have.been.calledWithMatch',
+      new RegExp(`${37.5}.*${48.01}.*${encodeURIComponent('John Doe')}`)
+    );
   });
 });
