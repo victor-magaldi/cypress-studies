c8dbdd185fea614ee09c6d92883025658f1b828e
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-09-17 20:31:51 -0300

    test: should share a location URL

--- project-04/cypress/e2e/location.cy.js
+++ project-04/cypress/e2e/location.cy.js
@@ -1,5 +1,5 @@
 describe('share location', () => {
-  it('should fetch the user location', () => {
+  beforeEach(() => {
     cy.visit('/').then((win) => {
       cy.stub(win.navigator.geolocation, 'getCurrentPosition')
         .as('getUserPosition')
@@ -14,10 +14,18 @@ describe('share location', () => {
           }, 100);
         });
     });
+  });
+  it('should fetch the user location', () => {
     cy.get('[data-cy="get-loc-btn"]').click();
     cy.get('@getUserPosition').should('have.been.called');
     cy.get('[data-cy="get-loc-btn"]').should('be.disabled');
     cy.get('[data-cy="get-loc-btn"]').should('be.disabled');
     cy.get('[data-cy="actions"]').should('contain', 'Location fetched');
+  });
+
+  it('should share a location URL', () => {
+    cy.visit('/');
+    cy.get('[data-cy="name-input"').type('John Doe');
+    cy.get('[data-cy="get-loc-btn"').click();
   });
 });
