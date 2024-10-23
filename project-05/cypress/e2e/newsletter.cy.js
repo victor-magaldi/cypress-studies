cfaa8ae3918992f7631af98e3db336f2ea56890b
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-10-23 20:42:25 -0300

    feat: creating interceptors and waiting your alias

--- project-05/cypress/e2e/newsletter.cy.js
+++ project-05/cypress/e2e/newsletter.cy.js
@@ -4,9 +4,14 @@ describe('Newsletter', () => {
   });
 
   it('should display a success message', () => {
+    cy.intercept('POST', '/newsletter*', {
+      statusCode: 201,
+      body: { status: 201 },
+    }).as('subscribe'); // dummy response
     cy.visit('/');
     cy.get('[data-cy="newsletter-email"]').type('test@example.com');
     cy.get('[data-cy="newsletter-submit"]').click();
+    cy.wait('@subscribe'); // await intercept with dummy data
     cy.contains('Thanks for signing up!');
   });
 });
